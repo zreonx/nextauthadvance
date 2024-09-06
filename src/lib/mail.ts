28d0bd9064418f6604692fbcc93b479f44934e9a
Zreon <56314835+zreonx@users.noreply.github.com>
2024-09-07 00:10:08 +0800

    added mailtrap as smtp server

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -10,21 +10,36 @@ export async function sendMail({
   body: string;
 }) {
   //Setup SMTP Server
-  const { SMTP_EMAIL, SMTP_GMAIL_PASS } = process.env;
+  const { SMTP_EMAIL, SMTP_GMAIL_PASS, SMTP_USER, SMTP_PASS } = process.env;
+
+  //Gmail SMTP SERVER
+  //   const transport = nodemailer.createTransport({
+  //     service: "gmail",
+  //     auth: {
+  //       user: SMTP_EMAIL,
+  //       pass: SMTP_GMAIL_PASS,
+  //     },
+  //   });
+
+  //Mail Trap SMTP SERVER
   const transport = nodemailer.createTransport({
-    service: "gmail",
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
     auth: {
-      user: SMTP_EMAIL,
-      pass: SMTP_GMAIL_PASS,
+      user: SMTP_USER,
+      pass: SMTP_PASS,
     },
   });
 
+  //Test Connections
   try {
     const testResult = await transport.verify();
     console.log("test result of transport: ", testResult);
   } catch (error) {
     console.error(error);
   }
+
+  //Sending Email
 
   try {
     const sendResult = await transport.sendMail({
